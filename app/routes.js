a104331ffdfaa6198ba2ba22da7ae35b91ce2ede
Kimberly Lewis <kblewis.flt@gmail.com>
2016-10-27 16:17:35 -0600

    routes setup, backend done maybe

---
+++ app/routes.js
@@ -0,0 +1,25 @@
+//grab first model
+var Nerd = require('./models/nerd');
+
+    module.exports = function(app) {
+        //server routes
+        //handle api calls
+        //authentication routes
+
+        app.get('/api/nerds', function(req, res) {
+           Nerd.find(function (err, nerds) {
+              if (err)
+                res.send(err);
+               res.json(nerds);
+           });
+        });
+
+        //app.post goes here
+        //app.delete goes here
+
+        //frontend routes ================
+        //route to handle all angular requests
+        app.get('*', function(req, res){
+            res.sendfile('./public/views/index.html'); //loads index page
+        });
+    };
