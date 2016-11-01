3564d7a1b4376edcd126507af293222865a05a1b
Kimberly Lewis <kblewis.flt@gmail.com>
2016-11-01 13:45:22 -0600

    things here

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,3 +1,20 @@
-/**
- * Created by kimberlylewis on 11/1/16.
- */
+angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        })
+        .when('/nerds', {
+            templateUrl: 'views/nerd.html',
+            controller: 'NerdController'
+        })
+        .when('/login', {
+           templateUrl: 'views/login.html',
+           controller: 'LoginController'
+        })
+        .when('/about', {
+           templateUrl: 'views/about.html',
+           controller: 'AboutController'
+        });
+    $locationProvider.html5Mode(true);
+}]);
